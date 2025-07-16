f6622b94c108659930335177582ef34f0fcaa01f
Madhav Agarwal <agarwalmadhav1411@gmail.com>
2025-07-16 21:16:54 +0530

    Add initial route handlers for auth, user, and subscription
    Created basic Express routers for authentication, user, and subscription endpoints with placeholder responses. These files establish the structure for future implementation of API logic.

---
+++ routes/auth.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const authRouter = Router();
+
+authRouter.post("/sign-up", (req, res) => { res.send({title: "Sign Up Endpoint"}); });
+authRouter.post("/sign-in", (req, res) => { res.send({title: "Sign in Endpoint"}); });
+authRouter.post("/sign-out", (req, res) => { res.send({title: "Sign out Endpoint"}); });
+// authRouter.post("/sign-up", (req, res) => { res.send({title: "Sign Up Endpoint"}); });
+
+export default authRouter;
