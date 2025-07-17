c7d471ecc369e8275227099896e3d0adc1359315
Madhav Agarwal <agarwalmadhav1411@gmail.com>
2025-07-17 22:23:25 +0530

    Add spacing between auth route definitions
    Blank lines were added between route definitions in auth.routes.js to improve readability and code organization.

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,8 +3,11 @@ import { Router } from "express";
 const authRouter = Router();
 
 authRouter.post("/sign-up", (req, res) => { res.send({title: "Sign Up Endpoint"}); });
+
 authRouter.post("/sign-in", (req, res) => { res.send({title: "Sign in Endpoint"}); });
+
 authRouter.post("/sign-out", (req, res) => { res.send({title: "Sign out Endpoint"}); });
+
 // authRouter.post("/sign-up", (req, res) => { res.send({title: "Sign Up Endpoint"}); });
 
 export default authRouter;
