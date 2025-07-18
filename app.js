cf182473b8573563dcb86d89df781f36711ad4ca
Madhav Agarwal <agarwalmadhav1411@gmail.com>
2025-07-18 17:51:54 +0530

    Add subscription routes and mount API endpoints
    Implemented CRUD and utility endpoints for subscriptions in subscription.routes.js. Updated app.js to mount user, auth, and subscription routes under versioned API paths.

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 import express from "express";
 import { PORT } from "./config/env.js";
 
+import userRoutes from "./routes/auth.routes.js";
+import subscriptionRoutes from "./routes/subscription.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+
+
 const app = express();
+
+app.use("/app/v1/auth", authRoutes);
+app.use("/app/v1/users", userRoutes);
+app.use("/app/v1/subscriptions", subscriptionRoutes);
 
 app.get("/", (req, res) => {
     res.send("welcome to the subscription tracker api");
