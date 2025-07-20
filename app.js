8104cde8e4390c8f1eb54e4ef768d816ca768473
Madhav Agarwal <agarwalmadhav1411@gmail.com>
2025-07-20 18:34:54 +0530

    Add MongoDB integration with Mongoose
    Introduced MongoDB support by adding mongoose and mongodb dependencies, creating a database connection module, and updating environment configuration to include DB_URI. The app now connects to the database on startup. Also updated ESLint config for Node.js globals and cleaned up README navigation.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { PORT } from "./config/env.js";
 import userRoutes from "./routes/auth.routes.js";
 import subscriptionRoutes from "./routes/subscription.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 
 const app = express();
@@ -16,8 +17,9 @@ app.get("/", (req, res) => {
     res.send("welcome to the subscription tracker api");
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, async ()=>{
     console.log(`Subscription tracker api is running on http://localhost/${PORT}`);
+    await connectToDatabase();
 });
 
 export default app;
