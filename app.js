93782ed8fddeabd4dfc32da447cfb3e8c91657a6
Madhav Agarwal <agarwalmadhav1411@gmail.com>
2025-07-23 17:47:10 +0530

    Add global error handling middleware
    Introduced errorMiddleware to handle errors across the app, including Mongoose-specific errors. Registered the middleware in app.js and added cookie-parser. Also enabled JSON and URL-encoded body parsing.

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ import userRoutes from "./routes/auth.routes.js";
 import subscriptionRoutes from "./routes/subscription.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import connectToDatabase from "./database/mongodb.js";
-
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser)
 
 app.use("/app/v1/auth", authRoutes);
 app.use("/app/v1/users", userRoutes);
 app.use("/app/v1/subscriptions", subscriptionRoutes);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
     res.send("welcome to the subscription tracker api");
